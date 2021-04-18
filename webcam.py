f52fe103085d25f9927b5fb43297160b922d6229
Samuel Proulx <samuelproulx26@gmail.com>
2021-04-17 20:29:46 -0400

    old changes

--- webcam.py
+++ webcam.py
@@ -1,17 +1,17 @@
-import cv2
-
+import cv2 as cv
+import numpy as np
 
 def getStream(source):
-    stream = cv2.VideoCapture(source)
+    stream = cv.VideoCapture(source)
 
     while True:
         success, frame = stream.read()
-        cv2.imshow("Frame", frame)
-        k = cv2.waitKey(1)
+        cv.imshow("Frame", frame)
+        k = cv.waitKey(1)
         if k == 27:
             break
 
     stream.release()
-    cv2.destroyAllWindows()
+    cv.destroyAllWindows()
 
 getStream(0)
